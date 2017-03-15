64af79d15d146b24b19603d6fb86ceccefc6335c
Luke Wegryn <luwegryn@cisco.com>
2017-03-15 10:53:50 -0400

    Added tests and test running

--- up/test/test.js
+++ up/test/test.js
@@ -1,8 +1,18 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var expect = require('expect'),
+    request = require('request')
+
+var port = 3000
+
+describe('GET', function() {
+  describe('GET /', function() {
+    it('should return 200 when a GET request is sent to /', function() {
+      request
+        .get('http://127.0.0.1:'+port+'/')
+        .on('response', function(res){
+          expect(res.status).to.equal(200)
+          done()
+        });
     });
   });
 });
+
